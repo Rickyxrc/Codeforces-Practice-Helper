8f537d4da7d60a18c122b0aeec64d61a0e2c573f
Ricky <81732554+Rickyxrc@users.noreply.github.com>
2023-09-07 20:39:16 +0800

    lint:tools.py 对代码进行 lint

--- tools.py
+++ tools.py
@@ -1,14 +1,32 @@
-import json,math,datetime,argparse,requests
+"""
+Codeforces Practice Helper - rickyxrc
+MIT licenced
+"""
+
+import json
+import math
+import datetime
+import argparse
+import requests
 from colorama import Fore,Style
 
 # parse argument
 
-parser = argparse.ArgumentParser(description="Codeforces Practice Helper - by " + Fore.GREEN + "rickyxrc" + Style.RESET_ALL,prog='helper.py')
+parser = argparse.ArgumentParser(description=
+                                 f"Codeforces Practice Helper -\
+                                      by {Fore.GREEN}rickyxrc{Style.RESET_ALL}"
+                                 ,prog='helper.py')
 
-parser.add_argument("--handle",help="specify user's handle",required=True,type=str)
-parser.add_argument("--fetch-problem",help="manually fetch the problem",required=False,action='store_true')
-parser.add_argument("--count-min",help="specify the minium problem tried to calculate rating",required=False,type=int,default=50)
-parser.add_argument("--ignore-labels",help="specify the ignored labels",required=False,type=list,default=[
+parser.add_argument("--handle",help="specify user's handle"
+                    ,required=True,type=str)
+parser.add_argument("--fetch-problem",
+                    help="manually fetch the problem",
+                    required=False,action='store_true')
+parser.add_argument("--count-min",
+                    help="specify the minium problem tried to calculate rating",
+                    required=False,type=int,default=50)
+parser.add_argument("--ignore-labels",
+                    help="specify the ignored labels",required=False,type=list,default=[
     "meet-in-the-middle",
     "chinese remainder theorem",
     "expression parsing",
@@ -16,9 +34,13 @@ parser.add_argument("--ignore-labels",help="specify the ignored labels",required
     "ternary search",
     "schedules"
 ])
-parser.add_argument("--max-rating-delta",help="specify the maximum rating delta for one submission",required=False,type=float,default=200)
-parser.add_argument("--initial-rating",help="specify the initial rating",required=False,type=int,default=300)
-parser.add_argument("--max-wrong-attempt",help="specify fetch the max wrong attempt",required=False,type=int,default=5)
+parser.add_argument("--max-rating-delta",
+                    help="specify the maximum rating delta for one submission",
+                    required=False,type=float,default=200)
+parser.add_argument("--initial-rating",
+                    help="specify the initial rating",required=False,type=int,default=300)
+parser.add_argument("--max-wrong-attempt",
+                    help="specify fetch the max wrong attempt",required=False,type=int,default=5)
 
 group = parser.add_mutually_exclusive_group(required=True)
 
@@ -26,8 +48,12 @@ group.add_argument("--show",help="show user's rating graph in command line",acti
 group.add_argument("--recent",help="show user's recent status",type=int)
 group.add_argument("--predict",help="predict user's perfomance in a single problem",type=str)
 group.add_argument("--predict-contest",help="predict user's perfomance in a contest",type=int)
-group.add_argument("--suggest",help="suggest some problem in a range of accept probability (--suggest prob_min:float prob_max:float number:int)",type=float,nargs=3)
-group.add_argument("--suggest-show",help="suggest some problem in a range of accept probability and show tags of them",type=float,nargs=3)
+group.add_argument("--suggest",
+                   help="suggest some problem in a range of accept probability \
+                   (--suggest prob_min:float prob_max:float number:int)",
+                   type=float,nargs=3)
+group.add_argument("--suggest-show",help="suggest some problem in a range of \
+                   accept probability and show tags of them",type=float,nargs=3)
 group.add_argument("--fetch",help="fetch the submissions from the user",action='store_true')
 
 arg = parser.parse_args()
@@ -41,6 +67,7 @@ ignored_labels = arg.ignore_labels
 count_min = arg.count_min
 initial_rating = arg.initial_rating
 max_wrong_attempt = arg.max_wrong_attempt
+max_rating_delta = arg.max_rating_delta
 
 # global temp variables
 
@@ -53,16 +80,28 @@ tried = {}
 # tools function
 
 def predict(now_rating:float,problem_rating:float) -> float:
+    """
+    elo rating 基础计算公式
+    通过现在 rating 和 题目rating 预测通过率
+    """
     return 1/(1+math.pow(10,(problem_rating-now_rating)/400))
 
 def update(now_rating:float,problem_rating:float,result:bool) -> float:
+    """
+    elo rating 基础计算公式
+    通过现在 rating 和 题目rating 以及结果更新rating
+    """
     global max_rating_delta
     return now_rating + max_rating_delta * (result - predict(now_rating,problem_rating))
 
 def update_tuple(now:tuple,problem_rating:float,result:bool) -> tuple:
+    """
+    用 tuple 的格式更新
+    """
     return (update(now[0],problem_rating,result),now[1]+1)
 
 def update_tag(problem_id:str,tag:str,result:bool) -> float:
+    """通过题目id，题目标签和结果算出rating变化量"""
     global ratings,problems
     try:
         problem_object = problems[problem_id]
@@ -74,19 +113,21 @@ def update_tag(problem_id:str,tag:str,result:bool) -> float:
     except KeyError:
         print('problem '+problem_id+'don\'t have rating judged,ignored.')
         return
-    pre_rating = ratings.get(tag,(initial_rating,0))[0]
     ratings[tag] = update_tuple(ratings.get(tag,(initial_rating,0)),problem_rating,result)
 
-def display_line(len:int,process:float)->str:
-    res = list(' '*len)
-    for i in range(int(len*process)):
+def display_line(length:int,process:float)->str:
+    """显示一行"""
+    res = list(' '*length)
+    for i in range(int(length*process)):
         res[i] = '#'
     return ''.join(res)
 
 def display(rating:float)->str:
+    """显示进度条"""
     return '['+display_line(100,rating/4000)+']'
 
 def get_col_from_rating(rating:float) -> str:
+    """显示 rating 颜色"""
     if rating < 1200:
         return Fore.BLACK
     if rating < 1400:
@@ -102,74 +143,81 @@ def get_col_from_rating(rating:float) -> str:
     return Fore.RED
 
 def fetch_problem():
+    """从 Codeforces 拉取处理题目信息"""
     proceed = {}
     contests = {}
     res = requests.get('https://codeforces.com/api/problemset.problems').json()
 
-    with open("problem.json","w") as write_stream:
+    with open("problem.json","w",encoding="utf-8") as write_stream:
         write_stream.write(json.dumps(res))
 
-    for r in res['result']['problems']:
+    for result in res['result']['problems']:
         try:
-            if r['type'] == 'PROGRAMMING' and not 'interactive' in r['tags'] and not '*special' in r['tags']:
-                proceed[str(r['contestId'])+r['index']] = {
-                        'rating' : r['rating'],
-                        'tags'   : r['tags'],
-                        'name'   : r['name']
+            if result['type'] == 'PROGRAMMING' and \
+                not 'interactive' in result['tags'] and not '*special' in result['tags']:
+                proceed[str(result['contestId'])+result['index']] = {
+                        'rating' : result['rating'],
+                        'tags'   : result['tags'],
+                        'name'   : result['name']
                     }
-                contests[r['contestId']] = contests.get(r['contestId'],[])
-                contests[r['contestId']].append(str(r['contestId'])+r['index'])
+                contests[result['contestId']] = contests.get(result['contestId'],[])
+                contests[result['contestId']].append(str(result['contestId'])+result['index'])
         except KeyError:
             pass
 
-    with open("contest.json","w") as write_stream:
+    with open("contest.json","w",encoding="utf-8") as write_stream:
         write_stream.write(json.dumps(contests))
 
-    with open("problem_proceed.json","w") as write_stream:
+    with open("problem_proceed.json","w",encoding="utf-8") as write_stream:
         write_stream.write(json.dumps(proceed))
 
 def fetch_profile(codeforces_user_handle:str):
-    profile_json = f"./_users/profile_{codeforces_user_handle}_generated.json"
-    request_url = f"https://codeforces.com/api/user.status?handle={codeforces_user_handle}&count=10000"
-    dat = requests.get(request_url).json()
+    """从 Codeforces 刷新用户信息"""
+    profile_json = f"./_users/profile_{codeforces_user_handle}" \
+                        + "_generated.json"
+    request_url = "https://codeforces.com/api/user.status?" \
+          + "handle={codeforces_user_handle}&count=10000"
+    user_profile_json = requests.get(request_url,timeout=10).json()
     lis = []
-    for k in dat['result']:
-        if k['verdict'] == 'TESTING':
-            print('ignored testing submission',k)
-            pass
+    for profile in user_profile_json['result']:
+        if profile['verdict'] == 'TESTING':
+            print('ignored testing submission',profile)
         try:
-            if k['passedTestCount'] == 0:
+            if profile['passedTestCount'] == 0:
                 continue
             res = {
-                'problemId':str(k['problem']['contestId'])+k['problem']['index'],
+                'problemId':str(profile['problem']['contestId'])+profile['problem']['index'],
                 'beat':False,
-                'time':k['creationTimeSeconds']
+                'time':profile['creationTimeSeconds']
             }
-            if k['verdict'] == 'OK':
+            if profile['verdict'] == 'OK':
                 res['beat'] = True
             lis.append(res)
         except KeyError:
-            print('ignored',k)
+            print('ignored',profile)
 
-    with open(profile_json,"w") as write_stream:
+    with open(profile_json,"w",encoding="utf-8") as write_stream:
         write_stream.write(json.dumps({
             "username" : codeforces_user_handle,
             "contest_history" : lis
             }))
-    print(f'total {len(dat["result"])}')
+    print(f'total {len(user_profile_json["result"])}')
 
 def print_ratings(colored = True,color_delta = False):
+    """
+    图形化地输出 rating 并排行
+    """
     global latest_rating
     rating_result = sorted(ratings.items(),key = lambda x:x[1],reverse = True)
     mul = 1
-    sum = 0
+    rating_sum = 0
     num = 0
     cnt_valid = 0
     printed_row = 0
     for rating in rating_result:
         if rating[1][1] > count_min:
             mul *= rating[1][0]
-            sum += rating[1][0]
+            rating_sum += rating[1][0]
             num += rating[1][1]
             cnt_valid += 1
 
@@ -183,42 +231,45 @@ def print_ratings(colored = True,color_delta = False):
                     control = Fore.RED
 
             clear = Style.RESET_ALL
-        
+
             pref = get_col_from_rating(rating[1][0])
-            
+
             if not color_delta:
                 control = pref
 
             if control == '' and rating[1][1] < count_min:
                 control = Fore.BLACK
 
-        print(f"{rating[1][1]:5} {pref}{rating[0]:30}{clear} {control}{round(rating[1][0],1):7} {display(rating[1][0])}{clear}")
+        print(f"{rating[1][1]:5} {pref}{rating[0]:30}{clear} \
+              {control}{round(rating[1][0],1):7} {display(rating[1][0])}{clear}")
         printed_row+=1
 
     if cnt_valid:
         mul = math.pow(mul,1/cnt_valid)
-        sum = sum / cnt_valid
+        rating_sum = rating_sum / cnt_valid
         control = ''
         if colored:
             control = get_col_from_rating(mul)
         clear = Style.RESET_ALL
-        print(f"\n{control}{num:5} {'rating (geometric mean)':30} {round(mul,1):7}{clear} {display(mul)}")
+        print(f"\n{control}{num:5} {'rating (geometric mean)':30} \
+              {round(mul,1):7}{clear} {display(mul)}")
         printed_row+=2
         if colored:
-            control = get_col_from_rating(sum)
-        print(f"{control}{num:5} {'rating (average)':30} {round(sum,1):7}{clear} {display(sum)}")
+            control = get_col_from_rating(rating_sum)
+        print(f"{control}{num:5} {'rating (average)':30} \
+              {round(rating_sum,1):7}{clear} {display(rating_sum)}")
         printed_row+=1
         latest_rating = ratings.copy()
 
 def win_rate(problem_id):
-    global ignored_labels
+    """通过题目获取胜率，采用几何平均数"""
     ans = 1
     cnt = 0
     problem_object = problems[problem_id]
-    for t in problem_object['tags']:
-        if t in ignored_labels:
+    for problem in problem_object['tags']:
+        if problem in ignored_labels:
             continue
-        my_rating = ratings.get(t,(initial_rating,0))[0]
+        my_rating = ratings.get(problem,(initial_rating,0))[0]
         ans *= predict(my_rating,problem_object['rating'])
         cnt += 1
     if cnt == 0:
@@ -226,35 +277,46 @@ def win_rate(problem_id):
     return math.pow(ans,1/cnt)
 
 
-def print_predict_problem(problemid:str):
+def print_predict_problem(problemid:str) -> None:
+    """输出题目的通过率"""
     clear = Style.RESET_ALL
-    print(f'rating of problem {get_col_from_rating(problems[problemid]["rating"])}{problemid:6}{clear} is {get_col_from_rating(problems[problemid]["rating"])}{str(round(problems[problemid]["rating"],1)):5}{clear} , your accepted rate is {str(round(win_rate(problemid)*100,1)):4}% (',end='')
-    for t in problems[problemid]['tags']:
-        if not t in ignored_labels:
-            print(f' {get_col_from_rating(ratings.get(t,(arg.initial_rating,0))[0])}{t} {round(predict(ratings.get(t,(arg.initial_rating,0))[0],problems[problemid]["rating"]),1)*100:3.2f}%{clear} ',end='')
+    print(f'rating of problem {get_col_from_rating(problems[problemid]["rating"])}{problemid:6}{clear}' \
+            + f' is {get_col_from_rating(problems[problemid]["rating"])}' \
+            + f'{str(round(problems[problemid]["rating"],1)):5}{clear}' \
+            + f', your accepted rate is {str(round(win_rate(problemid)*100,1)):4}% (',end='')
+    for problem in problems[problemid]['tags']:
+        if not problem in ignored_labels:
+            print(f' {get_col_from_rating(ratings.get(problem,(arg.initial_rating,0))[0])}{problem} {round(predict(ratings.get(problem,(arg.initial_rating,0))[0],problems[problemid]["rating"]),1)*100:3.2f}%{clear} ',end='')
     print(')')
     if accepted.get(problemid,False):
         print('You have '+Fore.GREEN+'Accepted'+Style.RESET_ALL+' this problem.')
     elif tried.get(problemid,False):
         print('You have '+Fore.GREEN+'Tried'+Style.RESET_ALL+' this problem.')
 
-def suggest_problem(win_rate_min:float,win_rate_max:float,limit:int=None,show_more:bool=False)->None:
-    print(f"suggest these problem for you (accept prob between {win_rate_min*100:3.2f}% and {win_rate_max*100:3.2f}%)")
+def suggest_problem(win_rate_min:float,win_rate_max:float,\
+                    limit:int=None,show_more:bool=False) -> None:
+    """
+    推荐通过率在win_rate_min到win_rate_max之间的题目
+    用limit限制数量，show_more控制是否显示详细信息
+    """
+    print(f"suggest these problem for you (accept prob between "\
+          + "{win_rate_min*100:3.2f}% and {win_rate_max*100:3.2f}%)")
     cnt = 0
     for i in problems:
         if i in problem_submited:
             continue
         rate = win_rate(i)
-        if rate >= win_rate_min and rate <= win_rate_max:
+        if win_rate_min <= rate <= win_rate_max:
             if not show_more:
                 print(f"{i:6} {rate*100:3.2f}%")
             else:
                 print_predict_problem(i)
             cnt+=1
-            if limit != None and cnt == limit:
+            if limit is not None and cnt == limit:
                 break
 
 def welcome_message():
+    """显示用户欢迎信息，可能会添加更多"""
     print(f"Welcome,user {dat['username']}.")
 
 if arg.fetch_problem:
@@ -262,24 +324,23 @@ if arg.fetch_problem:
 
 if not arg.fetch:
     try:
-        with open(profile_json,"r") as read_stream:
+        with open(profile_json,"r",encoding="utf-8") as read_stream:
             dat = json.loads(read_stream.read())
     except FileNotFoundError:
-        print(Fore.YELLOW+'user profile not found,fetching automatically.\nYou might run manually use --fetch few days later.'+Style.RESET_ALL)
+        print('{Fore.YELLOW}user profile not found,fetching automatically.\n'
+              + 'You might run manually use --fetch few days later.{Style.RESET_ALL}')
         fetch_profile(arg.handle)
-        with open(profile_json,"r") as read_stream:
+        with open(profile_json,"r",encoding="utf-8") as read_stream:
             dat = json.loads(read_stream.read())
     try:
-        with open('problem_proceed.json',"r") as read_stream:
+        with open('problem_proceed.json',"r",encoding="utf-8") as read_stream:
             problems = json.loads(read_stream.read())
     except FileNotFoundError:
         print(Fore.YELLOW+'problem_proceed.json not found,fetching automatically.\nYou might run manually use --fetch-problem few days later.'+Style.RESET_ALL)
         fetch_problem()
         print('problem fetched.')
-        with open('problem_proceed.json',"r") as read_stream:
+        with open('problem_proceed.json',"r",encoding="utf-8") as read_stream:
             problems = json.loads(read_stream.read())
-
-    max_rating_delta = arg.max_rating_delta
 
     dat['contest_history'].reverse()
 
@@ -293,14 +354,15 @@ if not arg.fetch:
         except KeyError:
             continue
 
-        if accepted.get(record['problemId'],None) == None or (not record['beat'] and tried.get(record['problemId'],0) > max_wrong_attempt):
+        if accepted.get(record['problemId'],None) is None or\
+              (not record['beat'] and tried.get(record['problemId'],0) > max_wrong_attempt):
 
             for single_tag in problem_tag:
                 if single_tag in ignored_labels:
                     continue
                 update_tag(record['problemId'],single_tag,record['beat'])
 
-            if record['beat'] == True:
+            if record['beat'] is True:
                 accepted[record['problemId']] = 1
             else:
                 tried[record['problemId']] = tried.get(record['problemId'],0) + 1
@@ -321,6 +383,7 @@ if not arg.fetch:
     # os.system("cls")
 
 def show_recent_status(limit:int=None):
+    """显示用户最近的刷题情况，使用 limit 限制数量"""
     print(f"{dat['username']}'s recent status:")
     for index,prob in enumerate(reversed(dat['contest_history'])):
         clear = ''
@@ -334,16 +397,16 @@ def show_recent_status(limit:int=None):
             control = Fore.GREEN
         else:
             control = Fore.RED
-        ti = datetime.datetime.fromtimestamp(prob['time'])
 
         try:
             col = get_col_from_rating(problems[prob["problemId"]]["rating"])
         except KeyError:
             col = ''
-        
-        print(f'{datetime.datetime.strftime(ti,"%Y-%m-%d, %H:%M:%S"):20} {col}{prob["problemId"]:6}{clear} {control}{res}{clear}')
-        
-        if limit != None and index+1 >= limit:
+
+        print(f'{datetime.datetime.strftime(datetime.datetime.fromtimestamp(prob["time"]),"%Y-%m-%d, %H:%M:%S"):20} ' +
+              f'{col}{prob["problemId"]:6}{clear} {control}{res}{clear}')
+
+        if limit is not None and index+1 >= limit:
             return
 
 if not arg.fetch:
@@ -353,7 +416,7 @@ if arg.show:
 if arg.predict:
     print_predict_problem(arg.predict)
 if arg.predict_contest:
-    with open("contest.json") as read_stream:
+    with open("contest.json",encoding="utf-8") as read_stream:
         for k in json.loads(read_stream.read())[str(arg.predict_contest)]:
             print_predict_problem(k)
 if arg.recent:
