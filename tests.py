32862a441b9a17eb918060da2a544575a63be712
Ricky <81732554+Rickyxrc@users.noreply.github.com>
2023-09-10 18:49:47 +0800

    fix:tests.py 修复损坏的测试

--- tests.py
+++ tests.py
@@ -14,7 +14,7 @@ class TestCodeforcesPractice:
     def test_rating_calculate(arg):
         with open("_users/profile_test_user_only_generated.json","w",encoding="utf-8") as write_stream:
             write_stream.write('{"username":"test_user_only","contest_history":[{"problemId":"1A","beat":false,"time":1000000000},{"problemId":"1A","beat":true,"time":1000000002}]}')
-        s = os.popen('python ./tools.py --handle test_user_only --show').read()
+        s = os.popen('python ./tools.py --handle test_user_only --show-all').read()
         assert('496.5' in s)
         assert('Welcome' in s)
         # assert(s=='Welcome,user test_user_only.\n    1 \x1b[30mmath                          \x1b[0m               \x1b[30m  496.5 [############                                                                                        ]\x1b[0m\n')
@@ -38,6 +38,9 @@ class TestCodeforcesPractice:
         s = os.popen('python ./tools.py --handle test_user_only --recent 2').read()
         assert('Accepted' in s)
         # assert(s=="Welcome,user test_user_only.\ntest_user_only's recent status:\n1970-01-01, 08:00:00 \x1b[30m1A    \x1b[0m \x1b[32mAccepted\x1b[0m\n")
+    def test_contest_predict(arg):
+        s = os.popen('python ./tools.py --handle test_user_only --suggest-contest 0.1 10 1').read()
+        assert('1859' in s)
 
 if __name__ == '__main__':
     pytest.main('')
